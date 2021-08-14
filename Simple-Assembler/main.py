895f334735e05365b5a03c47f82fdd08c9db556f
Antonio Pedro <antoio20028@iiitd.ac.in>
2021-08-14 13:18:30 +0530

    Add more instruction

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -1,7 +1,7 @@
 import sys
 
 regs = ["R0", "R1", "R2", "R3", "R4", "R5", "R6"]
-
+mem_addrs = ["[R0]", "[R1]", "[R3]", "[R3]", "[R4]", "[R5]", "[R6]"]
 
 type_A = ["add", "sub", "mul", "xor", "or", "and"]
 type_B = ["mov", "rs", "ls"]
@@ -10,6 +10,7 @@ type_D = ["ld", "st"]
 type_E = ["jmp", "jlt", "jgt", "je"]
 
 errors = []
+immediates = ["$%s"%i for i in range(0, 255)]
 
 def sub(input):
     pass
@@ -30,30 +31,92 @@ def ld (input):
 def st(input):
     pass
 
+def registerError(reg):
+    errors.append("%s is not a valid register "%reg)
+    
+def mem_addrError(reg):
+    errors.append("%s is not a valid mem_addr "%reg)
+    
+def argumentsError(args):
+    errors.append("%s is not a valid register "%args[i])
 
+def immediateError(imm):
+    errors.append("%s is not a valid immediate"%imm)
+    
+    
 def isTypeA(instruction, args):
     if instruction in type_A:
         if len(args) == 3:
             for i in range(len(args)):
-                if isValid_register(args[i]):
+                if isValid_register(args[i].strip()):
                     return True
-                
+                else:
+                    registerError(args[i])  
+        else:
+            errors.append("An type A instruction receives 3 arguments, but %s was given"%len(args))
                     
 def isTypeB(instruction, args):
     if instruction in type_B:
         if len(args) == 2:
-            if isValid_register(args[0]) and args[1] in range(0, 255):
+            if isValid_register(args[0]):
+                if isValid_immediate(args[1].strip()):
+                    return True
+                else:
+                    immediateError(args[1])
+            else:
+                registerError(args[0])
+        else:
+            errors.append("An type B instruction receives 2 arguments, but %s was given"%len(args))
+            
+def isTypeC(instruction, args):
+    if instruction in type_C:
+        if len(args) == 2:
+            for i in range(len(args)):
+                if isValid_register(args[i]):
+                    return True
+                else:
+                    registerError(args[i])
+        else:
+            errors.append("An type C instruction receives 2 arguments, but %s was given "%len(args))
+            
+def isTypeD(instruction, args):
+    if instruction in type_D:
+        if len(args) == 2:
+            if isValid_register(args[0]):
+                if isValid_mem_addr(args[1].strip()):
+                    return True
+                else:
+                    mem_addrError(args[1])
+            else:
+                registerError(args[0])      
+        else:
+            errors.append("An type D instruction receives 2 arguments but %s was given"%len(args))
+            
+def isTypeE(instruction, args):
+    if instruction in type_E:
+        if len(args) == 1:
+            if isValid_mem_addr(args[0].strip()):
                 return True
-
-
+            else:
+                mem_addrError(args[0])
+        else:
+            errors.append("An type E just receives 1 arguments, but %s was given"%len(args))
+            
 def isValid_register(arg):
     if arg in regs:
+        return True
+    
+def isValid_mem_addr(mem_addr):
+    if mem_addr in mem_addrs:
         return True
     
 def isValid_instruction(instruction):
     if instruction in type_A or  type_B or type_C or type_D or type_E:
         return True
     
+def isValid_immediate(imm):
+    if imm in immediates:
+        return True
 
 def switch_case(instruction, args):
    if instruction == "add":
@@ -63,21 +126,58 @@ def switch_case(instruction, args):
         return isTypeA(instruction, args)
         
    elif instruction == "div":
-       if len(args) == 2:
-           return True
+       return isTypeC(instruction, args)
+    
    elif instruction == "mul":
-       if len(args) == 3:
-           return True
-   elif instruction == "st":
-       if len(args) == 2:
-           return True
-   elif instruction == "ld":
-       if len(args) == 2:
-           return True
-   elif instruction == "mov":
-       if len(args) == 2:
-           return True
+      return isTypeC(instruction, args)
 
+    
+   elif instruction == "st":
+       return isTypeD(instruction, args)
+    
+   elif instruction == "ld":
+       return isTypeD(instruction, args)
+    
+   elif instruction == "mov":
+       return isTypeB(instruction, args)
+    
+   elif instruction == "mov":
+        return isTypeC(instruction, args)
+    
+   elif instruction == "rs":
+       return isTypeB(instruction, args)
+    
+   elif instruction == "ls":
+       return isTypeB(instruction, args)
+    
+   elif instruction == "xor":
+       return isTpeA(instruction, args)
+    
+   elif instruction == "or":
+       return isTypeA(instruction, args)
+    
+   elif instruction == "and":
+       return isTypeA(instruction, args)
+    
+   elif instruction == "not":
+       return isStypeC(instruction, args)
+    
+   elif instruction == "cmp":
+        return isTypeC(instruction, args)
+    
+   elif instruction == "jmp":
+       return isTypeE(instruction, args)
+    
+   elif instruction == "jlt":
+        return isTypeE(instruction, args)
+    
+   elif instruction == "jgt":
+       return isTypeE(instruction, args)
+    
+   elif instruction == "je":
+       return isTypeE(instruction, args)
+
+    
 def halt():
     sys.exit()
 
@@ -92,16 +192,7 @@ if __name__ == "__main__":
 
         if instruction.strip() != "hlt":
             if isValid_instruction(instruction.strip()):
-                if switch_case(instruction.strip(), args):
-                    for arg in args:
-                        if isValid_register(arg.rstrip("\n").strip()):
-                            flag = True
-                        else:
-                            errors.append("The %s is not a valid register"%arg)
-                          
-                else:
-                    errors.append("this instruction requires 3 arguments, but %s was given"%len(args))
-                
+                switch_case(instruction.strip(), args)
             else:
                 errors.append(instruction, " is not a valid instruction")
                 
@@ -110,6 +201,5 @@ if __name__ == "__main__":
             else:
                 for err in errors:
                     print(err)
-                    halt()
         else:
            halt()
