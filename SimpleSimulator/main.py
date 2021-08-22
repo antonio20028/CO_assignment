22b61c5a10f30115957844fbde6863a5069cac04
Antonio Pedro <antonio20028@iiitd.ac.in>
2021-08-22 14:21:24 +0530

    defenie skeleton

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -1,13 +1,24 @@
 import sys
+import operations
 
-MEM = {}
+mem = []
 program_counter = 0
-REG_FILE = []
+halted = False
+
+reg_file = {
+    "000": 0,
+    "001": 0,
+    "010": 0,
+    "011": 0,
+    "100": 0,
+    "101": 0,
+    "110": 0,
+    "111": 0}
 
 TYPE_A = ["00000", "00001", "00110", "01010", "01011", "01100"]
-TYPE_B = ["00000", "01000", "01001"]
+TYPE_B = ["00010", "01000", "01001"]
 TYPE_C = ["00111", "01101", "01110"]
-TYPE_D = ["001000", "00101"]
+TYPE_D = ["00100", "00101"]
 TYPE_E = ["01111", "10000", "10001", "10010"]
 TYPE_F = ["10011"]
 
@@ -17,50 +28,63 @@ def get_binary(value, width):
     res = bin(value)
     return res[2:].zfill(width)
 
-def decode_instruction(single_line):
+def initialize(single_line):
     opcode = single_line[:5]
     reg = []
+    instruction = dict()
 
     if opcode in TYPE_A:
         reg.append(single_line[7:10])
         reg.append(single_line[10:13])
         reg.append(single_line[13:16])
-        MEM[opcode] = reg
+        instruction[opcode] = reg
+        mem.append(instruction)
 
     elif opcode in TYPE_B:
         reg.append(single_line[7:10])
         reg.append(single_line[10:])
-        MEM[opcode] = reg
-
+        instruction[opcode] = reg
+        mem.append(instruction)
     
+    elif opcode in TYPE_C:
+        reg.append(single_line[10:13])
+        reg.append(single_line[13:16])
+        instruction[opcode] = reg
+        mem.append(instruction)
 
-#operations
-def sub(input):
-    pass
+    elif opcode in TYPE_D:
+        reg.append(single_line[7:10])
+        reg.append(single_line[10:])
+        instruction[opcode] = reg
+        mem.append(instruction)
 
-def add(input):
-    pass
+    elif opcode in TYPE_E:
+        reg.append(single_line[10:])
+        instruction[opcode] = reg
+        mem.append(instruction)
+    
+    else:
+        sys.exit()
 
-def div(input):
-    pass
+def exectute(instruction):
 
-def mul(input):
-    pass
+    return 0
 
-
-def ld (input):
-    pass
-
-def st(input):
-    pass
-
+def get_register_file():
+    for line in reg_file.keys():
+        print(reg_file[line])
 
 if __name__ == "__main__":
-    print("")
-
-    tmp = open ("/home/iiitd/Documents/assignment/CO_assignment/SimpleSimulator/test", "r")
-
-    for line in tmp.readlines():
-        print(decode_instruction(line))
     
-    print(MEM)
+    for line in sys.stdin:
+        if line == "":
+            break
+        initialize(line.strip())
+
+    while halted != True:
+        
+        instrunction = mem[program_counter]
+
+        program_counter = exectute(instrunction)
+
+        get_register_file()
