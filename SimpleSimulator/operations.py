703a2ec07803b5b81103c896b902ae5e930c5a0e
= <=>
2021-08-22 14:07:20 +0530

    initialize memory, operations and etc

---
+++ SimpleSimulator/operations.py
@@ -0,0 +1,23 @@
+#operations
+def sub(input):
+    pass
+
+def add(reg_1, reg_2):
+    
+    return reg_1 + reg_2
+
+def div(reg_1, reg_2):
+    
+    if reg_2 != 0:
+        return reg_1 // reg_2
+
+def mul(reg_1, reg_2):
+    
+    return reg_1 * reg_2
+
+def ld (r):
+    pass
+
+def st(input):
+    pass
+
