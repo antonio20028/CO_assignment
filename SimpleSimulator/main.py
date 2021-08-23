5dddb71bbe90571e5f5a34d7e69599ae8342cddc
= <antonio20028@iiitd.ac.in>
2021-08-23 17:14:20 +0530

    Add operations

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -17,7 +17,7 @@ reg_file = {
 
 TYPE_A = ["00000", "00001", "00110", "01010", "01011", "01100"]
 TYPE_B = ["00010", "01000", "01001"]
-TYPE_C = ["00111", "01101", "01110"]
+TYPE_C = ["00111", "01101", "01110", "00011"]
 TYPE_D = ["00100", "00101"]
 TYPE_E = ["01111", "10000", "10001", "10010"]
 TYPE_F = ["10011"]
@@ -70,15 +70,46 @@ def exectute(instruction):
 
     tmp  = list(instruction.keys())
     opcode = tmp[0]
+    flag = reg_file["111"]
 
     reg_address = instruction.get(opcode)[0]
 
     if opcode in TYPE_A:
         if opcode == TYPE_A[0]:
             reg_file[reg_address] = instruction.get(opcode)[1] + instruction.get(opcode)[2]
+        elif opcode == TYPE_A[1]:
+            if instruction.get(opcode)[1] > instruction.get(opcode)[2]:
+                reg_file[reg_address] = instruction.get(opcode)[1] - instruction.get(opcode)[2]
+            flag = "1"
+        elif opcode == TYPE_A[2]:
+            reg_file[reg_address] = instruction.get(opcode)[1] * instruction.get(opcode)[2]
+        elif opcode == TYPE_A[3]:
+            reg_file[reg_address] = instruction.get(opcode)[1] ^ instruction.get(opcode)[2]
+        elif opcode == TYPE_A[3]:
+            reg_file[reg_address] = instruction.get(opcode)[1] | instruction.get(opcode)[2]
+        elif opcode == TYPE_A[4]:
+            reg_file[reg_address] = instruction.get(opcode)[1] & instruction.get(opcode)[2]
     elif opcode in TYPE_B:
         if opcode == TYPE_B[0]:
             reg_file[reg_address] = instruction.get(opcode)[1]
+        elif opcode == TYPE_B[1]:
+            reg_file[reg_address] = reg_file[reg_address] >> instruction.get(opcode)[1]
+        elif opcode == TYPE_B[2]:
+            reg_file[reg_address] = reg_file[reg_address] << instruction.get(opcode)[1]
+    elif opcode in TYPE_C:
+        if opcode == TYPE_C[0]:
+            if instruction.get(opcode)[1] > 0:
+                reg_file["000"] = reg_address / instruction.get(opcode)[1]
+                reg_file["001"] = reg_address % instruction.get(opcode)[1]
+        elif opcode == TYPE_C[3]:
+            reg_file[reg_address] = instruction.get(opcode)[1]
+        elif opcode == TYPE_C[1]:
+            reg_file[reg_address] = ~(instruction.get(opcode)[1])
+    elif opcode in TYPE_D:
+        if opcode == TYPE_D[0]:
+            reg_file[reg_address] = reg_file[instruction.get(opcode)[1]]
+        elif opcode == TYPE_D[1]:
+            reg_file[instruction.get(opcode)[1]] = reg_file[reg_address]
     else:
         halted = True
 
@@ -99,9 +130,11 @@ if __name__ == "__main__":
         initialize(line.strip())
 
     
-    for program_counter in range(len(mem)):
+    for i in range(len(mem)):
+        program_counter = i
         exectute(mem[program_counter])
 
+    print(program_counter)
     print(reg_file)
     # while halted != True:
         
